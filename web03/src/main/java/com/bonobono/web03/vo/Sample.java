0ce27d4695186b0238a8b43f339514d593c23f8a
Administrator <Administrator@smart20103>
2019-02-08 10:55:41 +0900

    [임명심]Sample.java 파일 생성 vo객체

---
+++ web03/src/main/java/com/bonobono/web03/vo/Sample.java
@@ -0,0 +1,22 @@
+package com.bonobono.web03.vo;
+
+public class Sample {
+	private int sampleId;	//sample.sample_id(테이블.컬럼명)
+	private String sampleName;
+	public int getSampleId() {
+		return sampleId;
+	}
+	public void setSampleId(int sampleId) {
+		this.sampleId = sampleId;
+	}
+	public String getSampleName() {
+		return sampleName;
+	}
+	public void setSampleName(String sampleName) {
+		this.sampleName = sampleName;
+	}
+	@Override
+	public String toString() {
+		return "Sample [sampleId=" + sampleId + ", sampleName=" + sampleName + "]";
+	}
+}
